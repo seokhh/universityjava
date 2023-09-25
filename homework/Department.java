1a8eb8d60eb56f9a15573081ef1f458d6f57d10a
seokhh <119908334+seokhh@users.noreply.github.com>
2023-09-25 16:54:20 +0900

    2학기 3주차 과제

---
+++ homework/Department.java
@@ -0,0 +1,148 @@
+package javaprogramming2.week3.homework;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Department {
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Student> studentList = new ArrayList<>();
+    ArrayList<Lecture> lectureList = new ArrayList<>();
+
+    void run() {
+        readLectures();
+        readAllstudents();
+        search();
+    }
+
+    void readLectures() {
+        Lecture lec = null;
+        String code;
+        while (true) {
+            code = scan.next();
+            if (code.contentEquals("end"))
+                break;
+            lec = new Lecture(code);
+            lec.read(scan);
+            lectureList.add(lec);
+        }
+    }
+
+    void printAllLectures() {
+        for (Lecture lec : lectureList){
+            lec.print();
+        }
+    }
+
+    void readAllstudents() {
+        Student st = null;
+        int id;
+        while (true) {
+            id = scan.nextInt();
+            if (id == 0)
+                break;
+            st = new Student();
+            st.read(scan, id, this);
+            studentList.add(st);
+        }
+    }
+
+    void printAllstudents() {
+        for (Student st : studentList) {
+            st.print();
+        }
+    }
+
+    public Lecture findLecture(String code) {
+        for(Lecture lec: lectureList){
+            if(lec.matches(code))
+                return lec;
+        }
+        return null;
+    }
+
+    void search() {
+        String line = null;
+        String[] kwdArr = new String[0];
+        scan.nextLine();
+        while (true) {
+            System.out.print("메뉴선택\t(1) 학생 (2) 과목 (3) 연관검색 : ");
+            line = scan.nextLine();
+            if (line.equals("end"))
+                break;
+            if (line.equals("1"))
+                printAllstudents();
+            else if (line.equals("2"))
+                printAllLectures();
+            else if (line.equals("3")) {
+                System.out.print("연관검색 메뉴 (1) 과목별 수강생 (2) 요일별 수강생 (3) 타학년 과목 수강생수 조사 : ");
+                int n = scan.nextInt();
+                if (n == 1) {
+                    System.out.print("검색할 과목을 입력해주세요: ");
+                    String lectureCode = scan.next();
+                    Lecture foundLecture = findLecture(lectureCode);
+                    if (foundLecture != null) {
+                        System.out.println(foundLecture.name +" 수강생:");
+                        for (Student st : studentList) {
+                            if (st.registeredList.contains(foundLecture)) {
+                                System.out.print(st.name + " ");
+                            }
+                        }
+                        System.out.println();
+                    } else {
+                        System.out.println("과목을 찾을 수 없습니다.");
+                    }
+                } else if (n == 2) {
+                    System.out.print("검색할 요일을 입력해주세요: ");
+                    String day = scan.next();
+                    System.out.println(day +"요일 수강생:");
+                    for (Student st : studentList) {
+                        for (Lecture lec : st.registeredList) {
+                            if (lec.day.equals(day)) {
+                                System.out.print(st.name + " ");
+                                break;
+                            }
+                        }
+                    }
+                    System.out.println();
+                } else if (n == 3) {
+                    System.out.print("타학년을 검색할 학년을 입력해주세요: ");
+                    int yearToSearch = scan.nextInt();
+                    System.out.println(yearToSearch + "학년 학생 중에서 타학년 과목을 신청한 학생들:");
+
+                    for (Student st : studentList) {
+                        if (st.year == yearToSearch) {
+                            boolean found = false;
+                            for (Lecture lec : st.registeredList) {
+                                if (lec.year != yearToSearch) {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if (found) {
+                                System.out.print(st.name + " ");
+                            }
+                        }
+                    }
+                    System.out.println();
+                } else {
+                    System.out.println("잘못된 선택입니다.");
+                }
+                scan.nextLine();
+            } else {
+                kwdArr = line.trim().split(" ");
+                if (kwdArr.length > 0) { // Check if there are keywords
+                    for (Student st : studentList) {
+                        if (st.matches(kwdArr))
+                            st.print();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Department my = new Department();
+        my.run();
+    }
+}
---
+++ homework/Student.java
@@ -0,0 +1,61 @@
+package javaprogramming2.week3.homework;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Student {
+
+    String name;
+    int id;
+    String phone;
+    int year;
+    ArrayList<Lecture> registeredList = new ArrayList<>();
+
+    void read(Scanner scan, int id, Department department) {
+        this.id = id;
+        name = scan.next();
+        phone = scan.next();
+        year = scan.nextInt();
+        String code;
+        Lecture lec;
+        while(true){
+            code = scan.next();
+            if(code.contentEquals("0"))
+                break;
+            lec = department.findLecture(code);
+            registeredList.add(lec);
+        }
+    }
+
+    void print() { // Student
+        System.out.format("%d %s %s (%d학년) ", id, name, phone, year);
+        System.out.println();
+        for(Lecture lec: registeredList){
+            System.out.print("\t");
+            lec.print();
+        }
+    }
+
+    boolean matches(String kwd) {
+        // TODO Auto-generated method stub
+        if (name.contains(kwd))
+            return true;
+        if (kwd.length() >= 4 && phone.contains(kwd))
+            return true;
+        if (kwd.contentEquals("" + year))
+            return true;
+        return (kwd.length() > 4 && ("" + id).contains(kwd));
+    }
+
+    boolean matches(String[] kwdArr) {
+        // TODO Auto-generated method stub
+        for (String kwd : kwdArr) {
+            if (kwd.charAt(0) == '-') {
+                if (matches(kwd.substring(1)))
+                    return false;
+            } else if (!matches(kwd))
+                return false;
+        }
+        return true;
+    }
+}
